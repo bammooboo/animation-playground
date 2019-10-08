228d70437cc36f3781289c8e0a28c69cdc46de7c
Michaela <michaela.winterbottom@wemakewebsites.info>
2019-10-08 16:32:40 +0100

    WIP experimenting
    Experimeting with materials

--- src/js/animations/saturnLoop.js
+++ src/js/animations/saturnLoop.js
@@ -15,23 +15,30 @@ export default () => {
     //create inner spherical planet
     let geometry = new THREE.SphereGeometry(0.4, 30, 30);
     let material = new THREE.MeshBasicMaterial({color: 0x191919, wireframe: true});
-    planet = new THREE.Mesh(geometry, material);
+
+    let planetMaterial = new THREE.MeshLambertMaterial({color: 0x7fc5f9, emissive: 0x25673d, emissiveIntensity: 0.5});
+
+    let otherMaterial = new THREE.MeshPhongMaterial({color: 0x7fc5f9, emissive: 0x191919, emissiveIntensity: 0.5, shininess: 100, specular: 0x9d0a00});
+
+    let realMaterial = new THREE.MeshStandardMaterial({color: 0x7fc5f9, emissive: 0x191919, emissiveIntensity: 0.4, metalness: 1, roughness: 1});
+
+    planet = new THREE.Mesh(geometry, realMaterial);
     saturnScene.add(planet);
 
     // //create rings
     geometry = new THREE.TorusGeometry(0.51, 0.07, 2, 50);
-    material = new THREE.MeshBasicMaterial({color: 0x666666, wireframe: true});
-    let ring = new THREE.Mesh(geometry, material);
+    material = new THREE.MeshBasicMaterial({color: 0x666666});
+    let ring = new THREE.Mesh(geometry, otherMaterial);
     rings.push(ring);
 
     geometry = new THREE.TorusGeometry(0.69, 0.07, 2, 50);
-    material = new THREE.MeshBasicMaterial({color: 0x4c4c4c, wireframe: true});
-    ring = new THREE.Mesh(geometry, material);
+    material = new THREE.MeshBasicMaterial({color: 0x4c4c4c});
+    ring = new THREE.Mesh(geometry, otherMaterial);
     rings.push(ring);
 
     geometry = new THREE.TorusGeometry(0.85, 0.07, 2, 50);
-    material = new THREE.MeshBasicMaterial({color: 0x323232, wireframe: true});
-    ring = new THREE.Mesh(geometry, material);
+    material = new THREE.MeshBasicMaterial({color: 0x323232});
+    ring = new THREE.Mesh(geometry, otherMaterial);
     rings.push(ring);
 
     //to set correct perspective/angle of planet
@@ -48,6 +55,9 @@ export default () => {
 
     saturnCamera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1000);
     saturnCamera.position.z = 4;
+
+    let directionalLightUp = new THREE.DirectionalLight(0xffffff);
+    saturnScene.add(directionalLightUp);
 
     // let saturnAxes = new THREE.AxesHelper(5);
     // saturnScene.add(saturnAxes);
